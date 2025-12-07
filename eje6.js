ea0da55c3c8401468028e036692b1f5841924cf8
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-12-06 21:52:09 -0500

    Paso 6: url del sprite de charizard en consola

---
+++ eje6.js
@@ -0,0 +1,15 @@
+// Paso 6: 
+function mostrarSpriteCharizard() {
+  const url = "https://pokeapi.co/api/v2/pokemon/charizard";
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log("URL del sprite front_default de Charizard:", data.sprites.front_default);
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+}
+
+mostrarSpriteCharizard();
